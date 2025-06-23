1e45b14d078057b70964b6a811020cb457d7738f
Artur  Mkrtchyan <mkrtarturdev@gmail.com>
2025-06-23 11:58:00 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.10.0"),
-        .package(url: "https://github.com/SVGKit/SVGKit.git", from: "3.0.0")
+        .package(url: "https://github.com/arturdev/SVGKit.git", branch: "3.x")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
